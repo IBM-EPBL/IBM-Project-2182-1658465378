0284495679ce42efffe307d4d6eaadc6595ad687
THINHESH A C <86588309+thinhesh@users.noreply.github.com>
2022-11-18 00:28:53 +0530

    Add files via upload

---
+++ Project Development phase/Sprints 4/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, redirect, url_for, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        arr = []
+        for i in request.form:
+            val = request.form[i]
+            if val == '':
+                return redirect(url_for("home"))
+            arr.append(float(val))
+
+        # deepcode ignore HardcodedNonCryptoSecret: <please specify a reason of ignoring this>
+        API_KEY = "_y5HXZoBlP-pbdVhiMEyIryHoMWR2xyJcXeiSGPJlRuz"
+        token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={
+            "apikey": API_KEY, 
+            "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'
+            })
+        mltoken = token_response.json()["access_token"]
+        header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+        payload_scoring = {
+            "input_data": [{"fields":[  'GRE Score',
+                                        'TOEFL Score',
+                                        'University Rating',
+                                        'SOP',
+                                        'LOR ',
+                                        'CGPA',
+                                        'Research'], 
+                            "values": [arr]
+                            }]
+                        }
+
+        response_scoring = requests.post(
+            'https://jp-tok.ml.cloud.ibm.com/ml/v4/deployments/62772227-bda0-483c-9213-98ad2f100980/predictions?version=2022-11-17', 
+            json=payload_scoring,
+            headers=header
+        ).json()
+        
+        result = response_scoring['predictions'][0]['values']
+        
+        if result[0][0] > 0.5:
+            return redirect(url_for('chance', percent=result[0][0]*100))
+        else:
+            return redirect(url_for('no_chance', percent=result[0][0]*100))
+    else:
+        return redirect(url_for("home"))
+
+@app.route("/home")
+def demo2():
+    return render_template("home.html")
+
+@app.route("/chance/<percent>")
+def chance(percent):
+    return render_template("chance.html", content=[percent])
+
+@app.route("/nochance/<percent>")
+def no_chance(percent):
+    return render_template("noChance.html", content=[percent])
+
+@app.route('/<path:path>')
+def catch_all():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
